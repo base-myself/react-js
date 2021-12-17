2ad9a66c7aca25eeeafb9398a311a2dda8da74b7
Sotatek-MartinTung <cogentlabs.1@sotatek.com>
2021-12-17 17:04:49 +0700

    init

---
+++ src/pages/home/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./style.module.scss";
+
+const HomePage = () => <div className={styles.wrapper}>This is home page</div>;
+
+export default HomePage;
+
+HomePage.propTypes = {};
---
+++ src/pages/login/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./style.module.scss";
+
+const PageLogin = () => (
+  <div className={styles.wrapper}>This is login page</div>
+);
+
+export default PageLogin;
+
+PageLogin.propTypes = {};
